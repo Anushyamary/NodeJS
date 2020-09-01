e035ed35741347992e4d035dbcaa95f81363cfb0
Anu <anusofts4@gmail.com>
2020-09-01 23:55:34 +0530

    Node test2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-    res.send('Hello World')
+    res.send('Hello World Test')
 })
 app.listen(3000, function() {
     console.log('listening on 3000')
